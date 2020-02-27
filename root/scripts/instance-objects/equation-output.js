ac23807993b545d847ed24922ec97e428a684074
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2020-02-27 16:34:11 -0600

    New visual assets
    Placeholders for new visual assets have been created

---
+++ root/scripts/instance-objects/equation-output.js
@@ -0,0 +1,27 @@
+/**
+ * Creates an object that handles the animation of an equation.
+ * @param {HTMLCanvasElement} canvas where the function will be drawn.
+ */
+function EquationOutput(canvas)
+{
+    let equationSteps = [];
+
+    this.AddEquationStep = function(step)
+    {
+
+    };
+
+    this.AdvanceStep = function()
+    {
+
+    };
+    this.ReverseStep = function()
+    {
+
+    };
+
+    let update = function(deltaTime)
+    {
+        
+    }
+}
